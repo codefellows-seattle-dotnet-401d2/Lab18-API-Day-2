71152ba22e73e7446fc005f5ca42afe815572cc4
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-18 21:36:06 -0800

    wip

---
+++ CreateList/CreateList/Migrations/20180119052312_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CreateList.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskNew",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskNew", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskNew");
+        }
+    }
+}
---
+++ CreateList/CreateList/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreateList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
