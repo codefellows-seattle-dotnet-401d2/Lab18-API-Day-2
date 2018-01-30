b3d90ba39766869f88a522df14f369a5dcad24c5
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-29 22:08:23 -0800

    removed cache

---
+++ WebAppAPI2/WebAppAPI2/Controllers/ListController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppAPI2.Models;
+using WebAppAPI2.Data;
+
+namespace WebAppAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ListController : Controller
+    {
+        private readonly TaskListDbContext _context;
+
+        public ListController(TaskListDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/<controller>/
+        [HttpGet]
+        public IEnumerable<ListClass> Get()
+        {
+            return _context.Lists2;
+        }
+
+        // GET: api/<controller>/{id}
+        [HttpGet("{id:int}")]
+        public ListClass Get(int id)
+        {
+            ListClass myList = _context.Lists2.FirstOrDefault(x => x.Id == id);
+            if(myList != null)
+            {
+                myList.TaskList = _context.Tasks2.Where(x => x.ListClassId == id).ToList();
+            }
+            return myList;
+        }
+
+        // POST: api/<controller>/
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]ListClass value)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+            await _context.Lists2.AddAsync(value);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { value.Id }, value);
+        }
+
+        // PUT: api/<controller>/{id}
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, [FromBody]ListClass value)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+            value.Id = id;
+            if (_context.Lists2.FirstOrDefault(x => x.Id == id) != null)
+            {
+                _context.Lists2.Update(value);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                await Post(value);
+            }
+            return Ok();
+        }
+
+        // DELETE: api/<controller>/{id}
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            ListClass deleteList = _context.Lists2.FirstOrDefault(x => x.Id == id);
+            if (deleteList != null)
+            {
+                deleteList.TaskList = _context.Tasks2.Where(x => x.ListClassId == id).ToList();
+                foreach(TaskClass t in deleteList.TaskList)
+                {
+                    t.ListClassId = null;
+                    _context.Tasks2.Update(t);
+                }
+                _context.Lists2.Remove(deleteList);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ WebAppAPI2/WebAppAPI2/Models/TaskClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppAPI2.Models
+{
+    public class TaskClass
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime Time { get; set; }
+        [ForeignKey("ListClassId")]
+        public int? ListClassId { get; set; }
+    }
+}
